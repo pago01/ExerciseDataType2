e7e8b36a7de72353782e0479dd1df2ae6bc57b34
ACTFYZZ <ACTFYZZ@PL10006677>
2023-08-11 03:32:13 +0200

    Add project files.

---
+++ ExerciseDataType2/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+
+namespace ExerciseDatatype2
+{
+    //Data structure declaration
+
+    class Program
+    {
+        
+
+        public static void Main(string[] args)
+        {
+            char letter1 = ' ';
+            char letter2 = ' ';
+            char letter3 = ' ';
+
+            //Letter1
+            Console.WriteLine("Podaj pierwszą literę");
+            Char.TryParse(Console.ReadLine(), out letter1);
+            //Letter2
+            Console.WriteLine("Podaj drugą literę");
+            Char.TryParse(Console.ReadLine(), out letter2);
+            //Letter3
+            Console.WriteLine("Podaj trzecią literę");
+            Char.TryParse(Console.ReadLine(), out letter3);
+            
+            //Display data
+            Console.WriteLine($"Litera 3: {letter3}");
+            Console.WriteLine($"Litera 2: {letter2}");
+            Console.WriteLine($"Litera 1: {letter1}"); 
+            
+        }
+    }
+}
